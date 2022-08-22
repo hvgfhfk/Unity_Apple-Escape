a8a09520fc7236ded29dad89b2aae212eaec3cdb
Jun <70848796+hvgfhfk@users.noreply.github.com>
2022-08-22 12:37:41 +0900

    .
    .

---
+++ Assets/script/UseItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseItem : MonoBehaviour
+{
+    public void ButtonOnClick()
+    {
+        Debug.Log("on click");
+    }
+    public void SecondButtonClick()
+    {
+        Debug.Log("click");
+    }
+}
